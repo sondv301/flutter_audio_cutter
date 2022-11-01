864b1a67a8ef5df5bc8e91d7b22bda48b4bdbc47
Afjal <52181725+afjal1@users.noreply.github.com>
2022-11-01 17:14:06 +0530

    Update audio_cutter.dart
    Now It Trim any type of audio Extension

--- lib/audio_cutter.dart
+++ lib/audio_cutter.dart
@@ -4,10 +4,12 @@ import 'dart:io';
 import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
 
 class AudioCutter {
   /// Return audio file path after cutting
-  static Future<String> cutAudio(String path1, double start, double end) async {
+  static Future<String> cutAudio(
+      String path, String extension, double start, double end) async {
     if (start < 0 || end < 0) {
       throw ArgumentError('The starting and ending points cannot be negative');
     }
@@ -16,16 +18,14 @@ class AudioCutter {
           'The starting point cannot be greater than the ending point');
     }
 
-//  /storage/emulated/0/Android/data/com.example.voxpod/files/question1.mp4
-    final Directory dir = await getTemporaryDirectory();
-    var path = Platform.isAndroid
+    var directory = Platform.isAndroid
         ? await getExternalStorageDirectory()
         : await getApplicationSupportDirectory();
-    final outPath = "${path!.path}/trimmed.aac";
+    final outPath = "${directory!.path}/trimmed${extension}";
     await File(outPath).create(recursive: true);
 
     var cmd =
-        "-y -i \"$path1\" -vn -ss $start -to $end -ar 16k -ac 2 -b:a 96k -acodec copy $outPath";
+        "-y -i \"$path\" -vn -ss $start -to $end -ar 16k -ac 2 -b:a 96k -acodec copy $outPath";
 
     await FFmpegKit.execute(cmd);
 
